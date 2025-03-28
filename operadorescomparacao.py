e9afa8bbf0f34a1808a810f0f7514631d05f76b6
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 11:02:33 -0300

    operadores logicos

---
+++ operadorescomparacao.py
@@ -0,0 +1,23 @@
+
+x= 10
+y=10
+
+comparacao de igualdade
+print(x==y)
+
+# diferença
+print(x !=y)
+
+# Superioridade (>)
+print (x>y)
+# Inferioridade (<)
+print(x<y)
+# Maior ou igual (>=)
+print (x>=y)
+# Menor ou igual (<=)
+print (x<=y)
+
+
+
+
+

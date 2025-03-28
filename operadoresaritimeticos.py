fc8615f4bd13b8f8aa547226efdbaf4099363944
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-03-28 10:04:44 -0300

    operadores aritimeticos

---
+++ operadoresaritimeticos.py
@@ -0,0 +1,32 @@
+# Adiçao
+numero1 = 10
+numero2 = 20
+
+soma = numero1 + numero2
+print(soma)
+
+# Subtraçao
+
+subtraçao = numero1 - numero2
+print(subtraçao)
+
+# Multiplicaçao
+multiplicacao= numero1 * numero2
+print(multiplicacao)
+
+#divisao
+divisao = numero1/numero2
+print(divisao)
+# Potencia
+potencia=numero1**numero2
+print (potencia)
+# Modulo
+modulo=numero1%numero2
+print(modulo)
+
+# Resto da divisao
+restodadivisao= numero1 // numero2
+print (restodadivisao)
+
+
+
